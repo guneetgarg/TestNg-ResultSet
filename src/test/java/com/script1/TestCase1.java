de9f2b351160b4c54493d97463a3ac78129e7fd8
guneetgarg <guneetgarg@gmail.com>
2017-01-09 11:40:05 +0530

    rename

--- src/test/java/com/script1/TestCase1.java
+++ src/test/java/com/script1/TestCase1.java
@@ -2,37 +2,26 @@ package com.script1;
 
 import static org.testng.Assert.assertTrue;
 
+import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class TestCase1 {
-	
-	@Test(description = "test descriptionA")
+
+	@Test(description = "TestCase1 description A")
 	public void PageATest() {
 		assertTrue(true);
 	}
-	
+
 	@Test()
 	public void PageBTest() {
 		assertTrue(true);
 	}
 
-	@Test(groups = { "bonding", "strong_ties" })	//(description = "test descriptionC")
+	@Test(groups = { "bonding", "strong_ties" }) // (description = "test													// descriptionC")
 	public void PageCTest() {
 		assertTrue(true);
 	}
-
-
-	
-	/*@Test(description = "Lorem ipsum ds in aliquam bibendum")
-	public void Page1Test() {
-		Assert.assertEquals(true, false, "FAILED QWERTYUHGCZXCGHGFD");
-	}
-
-	@Test(description = "test description")
-	public void Page2Test() throws InterruptedException {
-		assertTrue(true);
-	}
-
 
 	@Test(description = "test description")
 	public void Page4Test() {
@@ -55,14 +44,4 @@ public class TestCase1 {
 		throw new SkipException("Skipping - This is not ready for testing ");
 	}
 
-	@Test(description = "test description")
-	public void Page4Test1() {
-		throw new SkipException("Skipping - This is not ready for testing ");
-	}
-
-	@Test(description = "test description")
-	public void Page4Test12() {
-		throw new SkipException("Skipping - This is not ready for testing ");
-	}
-*/
 }
--- src/test/java/com/script1/TestCase2.java
+++ src/test/java/com/script1/TestCase2.java
@@ -8,20 +8,19 @@ import org.testng.annotations.Test;
 
 public class TestCase2 {
 
-	@Test(testName = "Custom Google test name1")
-	public void GooglePage1Test(String name) {
+	@Test(testName = "TestCase2 Custom Google test name1", groups = "group TestCase2")
+	public void PageBTest1() {
 		Assert.assertEquals(true, false);
 	}
 
 	@Test
-	public void GooglePage2Test() {
+	public void PageBTest2() {
 		Reporter.log("Google WAS CALLED");
 		assertTrue(true);
 	}
 
-	@Test(testName = "Custom Google test name3")
-	public void GooglePage3Test() {
+	@Test(testName = "Custom Google test name3", groups = { "group TestCase2", "grp 1" })
+	public void PageBTest3() {
 		throw new SkipException("Skipping - This is not ready for testing ");
 	}
-
 }
